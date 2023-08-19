2ae6b1a20d6892d5562c47ff554754701723ce08
Nikola Bulat <nbulat99@gmail.com>
2023-08-19 16:08:29 +0200

    Update data_loader and upload data

--- data_loader.py
+++ data_loader.py
@@ -2,26 +2,56 @@ import tsplib95
 import math
 from common_functions import cycle_length
 
-def calculate_distance(coord1, coord2):
+def load_graph_data_euclidean(filepath):
+    problem = tsplib95.load(filepath)
+    coordinates = problem.node_coords
+
+    return create_adjacency_matrix_euclidean(coordinates)
+
+def load_graph_data_geo(filepath):
+    problem = tsplib95.load(filepath)
+    coordinates = problem.node_coords
+
+    return create_adjacency_matrix_geo(coordinates)
+
+def load_graph_data_full_matrix(filepath):
+    problem = tsplib95.load(filepath)
+    matrix = problem.edge_weights
+
+    return matrix
+
+def create_adjacency_matrix_euclidean(coordinates):
+    num_nodes = len(coordinates)
+    adjacency_matrix = [[0] * num_nodes for _ in range(num_nodes)]
+
+    for i in range(num_nodes):
+        for j in range(i + 1, num_nodes):
+            distance = calculate_distance_euclidean(coordinates[i + 1], coordinates[j + 1])  # Corrected indexing
+            adjacency_matrix[i][j] = distance
+            adjacency_matrix[j][i] = distance
+
+    return adjacency_matrix
+
+def calculate_distance_euclidean(coord1, coord2):
     xd = coord1[0] - coord2[0]
     yd = coord1[1] - coord2[1]
     distance = math.sqrt(xd * xd + yd * yd)
     rounded_distance = round(distance)  # Round to the nearest integer
     return rounded_distance
 
-def create_adjacency_matrix(coordinates):
+def create_adjacency_matrix_geo(coordinates):
     num_nodes = len(coordinates)
     adjacency_matrix = [[0] * num_nodes for _ in range(num_nodes)]
 
     for i in range(num_nodes):
         for j in range(i + 1, num_nodes):
-            distance = calculate_distance(coordinates[i + 1], coordinates[j + 1])  # Corrected indexing
+            distance = calculate_distance_geo(coordinates[i + 1], coordinates[j + 1])  # Using geo_distance function
             adjacency_matrix[i][j] = distance
             adjacency_matrix[j][i] = distance
 
     return adjacency_matrix
 
-def geo_distance(coord1, coord2):
+def calculate_distance_geo(coord1, coord2):
     PI = 3.141592
     RRR = 6378.388
     
@@ -49,41 +79,20 @@ def geo_distance(coord1, coord2):
     
     return dij
 
-def create_adjacency_matrix_geo(coordinates):
-    num_nodes = len(coordinates)
-    adjacency_matrix = [[0] * num_nodes for _ in range(num_nodes)]
-
-    for i in range(num_nodes):
-        for j in range(i + 1, num_nodes):
-            distance = geo_distance(coordinates[i + 1], coordinates[j + 1])  # Using geo_distance function
-            adjacency_matrix[i][j] = distance
-            adjacency_matrix[j][i] = distance
-
-    return adjacency_matrix
-
 def print_adjacency_matrix(adjacency_matrix):
     for row in adjacency_matrix:
         print(" ".join(f"{distance:2d}" for distance in row))
 
-def load_graph_data(filepath):
-    problem = tsplib95.load(filepath)
-
-    coordinates = problem.node_coords
-
-    return create_adjacency_matrix(coordinates)
-
-def load_graph_data_geo(filepath):
-    problem = tsplib95.load(filepath)
-
-    coordinates = problem.node_coords
-
-    return create_adjacency_matrix_geo(coordinates)
-
 def main():
-    adjacency_matrix = load_graph_data_geo('data/gr666.tsp')
+    # Cannonical tour to test the distance calculation
+    adjacency_matrix = load_graph_data_euclidean('data/pcb442.tsp')
 
     cannonical_tour_length = cycle_length(adjacency_matrix, list(range(len(adjacency_matrix))))
     print(cannonical_tour_length)
 
+    # Check the full matrix loading
+    print_adjacency_matrix(load_graph_data_full_matrix('data/bays29.tsp'))
+
+    
 if __name__ == "__main__":
     main()
